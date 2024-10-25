3b5baf3ea2eab8c22ac8df224fdcc15692baa643
matheusspacifico <matheuspacifico44@gmail.com>
2024-10-25 20:10:18 -0300

    delete page + css

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,8 +1,39 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import './styles/Delete.css';
 
 function Delete() {
+  const [id, setId] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleDelete(e){
+    e.preventDefault();
+
+    axios.delete(`https://6719816c7fc4c5ff8f4d9f11.mockapi.io/movies/${id}`)
+      .then(() => {
+        navigate("/");
+      }).catch((err) => {
+        console.error(err);
+        alert("ID entered was not found!");
+      });
+  }
+
   return (
-    <div>Delete</div>
+    <div className="delete-page">
+      <form className="modern-form" onSubmit={handleDelete}>
+        <h2>Delete Movie</h2>
+        <label>Enter Movie ID:</label>
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <button type="submit">Delete Movie</button>
+      </form>
+    </div>
   );
 }
 
